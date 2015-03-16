71231359172a13bc4744562ce1bdcb56cd25c9ab
Eric Wyne <ecwyne@gmail.com>
2015-03-16 13:20:36 -0700

    bump dep version

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'ecwyne:response-tap',
   summary: 'Simple extensible wrapper for responseTap API',
-  version: '0.0.9',
+  version: '0.0.10',
   git: 'https://github.com/ecwyne/meteor-response-tap'
 });
 
 Npm.depends({
-	"response-tap": '0.0.5'
+	"response-tap": '0.0.6'
 });
 
 Package.onUse(function(api) {

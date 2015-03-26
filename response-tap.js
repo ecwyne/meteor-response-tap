2f81f3ec43487374d9882018c4a09f84d4804b18
Eric Wyne <ecwyne@gmail.com>
2015-03-25 17:35:21 -0700

    change date range

--- response-tap.js
+++ response-tap.js
@@ -13,7 +13,7 @@ function formatPhone(phone){
 
 rTap.extend('searchByPhone', function (phone, callback){
 	var self = this;
-	var from = new Date(Date.now() - 1000*60*60*24*29).toISOString().substr(0,10);
+	var from = new Date(Date.now() - 1000*60*60*24*25).toISOString().substr(0,10);
 	var to = new Date(Date.now() + 1000*60*60*24*1).toISOString().substr(0,10);
 	phone = formatPhone(phone);
 	self.getCdrids(from, to, {comparisonValue: 'EXACT', type: 'CUSTOMER_NUMBER', value: phone}, function (err, obj){
